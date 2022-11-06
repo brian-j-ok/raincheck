4da87c1cb47fa425fcb54246510c42c4740a3df7
Brian O'Kelly <brianjrok@gmail.com>
2022-11-05 21:02:10 -0600

    check for rain (less robust than challenge, unable to access same level of data due to paywall on API in place now

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ def fetch_weather_data():
     response = requests.get(url=f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={config.api_key}")
     response.raise_for_status()
 
-    data = response.json()
-    print(data)
+    data = response.json()['weather']
+    print(data[0]['main'])
+    return data[0]['main']
 
 
-fetch_weather_data()
+def check_weather(data: str):
+    if data == 'rain':
+        print('raining today!')
+    else:
+        print('no rain today')
+
+
+check_weather(fetch_weather_data())
